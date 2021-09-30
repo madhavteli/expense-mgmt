0ab4157c1fa3d16566370c414b1aa2eb6643bba5
mteli <mteli@xento.com>
2021-09-30 14:17:30 +0530

    message

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -4,20 +4,20 @@ import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
-	const [title, setTitle] = useState(props.expenseDetails.title);
-
-	const buttonClickHandler = () => {
-		setTitle('Updated!');
-	};
+	// const [title, setTitle] = useState(props.expenseDetails.title);
+	//
+	// const buttonClickHandler = () => {
+	// 	setTitle('Updated!');
+	// };
 
 	return (
 		<div className={classes.expenseItem}>
 			<ExpenseDate date = {props.expenseDetails.date}/>
 			<div className={classes.expenseItemDescription}>
-				<h2>{title}</h2>
+				<h2>{props.expenseDetails.title}</h2>
 				<div className={classes.expenseItemPrice}>{props.expenseDetails.price}</div>
 			</div>
-			<button onClick={buttonClickHandler}>Change title</button>
+			{/*<button onClick={buttonClickHandler}>Change title</button>*/}
 		</div>
 	);
 };
